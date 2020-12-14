45d5ca910e616e31de998f2062bb5474b46f5b94
Yulia Mihaylova <yulu.2000@hotmail.com>
2020-12-14 20:31:18 +0300

    Segment class added

---
+++ Segment.java
@@ -0,0 +1,30 @@
+public class Segment {
+
+    private double left;
+    private double right;
+
+    public Segment() {
+
+    }
+
+    public Segment(double left, double right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public double getLeft() {
+        return left;
+    }
+
+    public void setLeft(double left) {
+        this.left = left;
+    }
+
+    public double getRight() {
+        return right;
+    }
+
+    public void setRight(double right) {
+        this.right = right;
+    }
+}
