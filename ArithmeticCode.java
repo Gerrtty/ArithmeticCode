59d62b2956214ea0a4cac814ad67abfc345dc0be
Yulia Mihaylova <yulu.2000@hotmail.com>
2020-12-14 18:47:25 +0300

    ArithmeticCode

---
+++ ArithmeticCode.java
@@ -0,0 +1,74 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArithmeticCode {
+
+    public static void main(String[] args) {
+
+        String s = "abacaba";
+
+        System.out.println(encode(s));
+    }
+
+    public static double encode(String s) {
+        HashMap<Character, Segment> segment = defineSegments(s);
+
+        double left = 0;
+        double right = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char symb = s.charAt(i);
+            double newRight = left + (right - left) * segment.get(symb).getRight();
+            left = left + (right - left) * segment.get(symb).getLeft();
+            right = newRight;
+        }
+
+        return (left + right) / 2;
+    }
+
+    public static HashMap<Character, Segment> defineSegments(String s) {
+        HashMap<Character, Double> map = getProbability(s);
+        Segment[] segment = new Segment[map.size()];
+
+        HashMap<Character, Segment> segmentMap = new HashMap<>();
+
+        double l = 0;
+
+        int i = 0;
+        for (Map.Entry<Character, Double> entry: map.entrySet()) {
+            segment[i] = new Segment();
+            segment[i].setLeft(l);
+            segment[i].setRight(l + entry.getValue());
+            l = segment[i].getRight();
+            segmentMap.put(entry.getKey(), segment[i]);
+            i++;
+        }
+
+        return segmentMap;
+    }
+
+    public static HashMap<Character, Double> getProbability(String s) {
+        char[] chars = s.toCharArray();
+        int len = s.length();
+
+        HashMap<Character, Double> map = new HashMap<>();
+
+        for (Character character : chars) {
+            Double count = map.get(character);
+
+            if (count == null || count == 0) {
+                map.put(character, 1.0);
+            } else {
+                map.put(character, count + 1);
+            }
+
+        }
+
+        for (Map.Entry<Character, Double> entry: map.entrySet()) {
+            entry.setValue(entry.getValue() / len);
+        }
+
+        return map;
+    }
+
+}
